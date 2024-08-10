fc8a637347f5b546590988741029d04420b436bb
siriravalid <152952587+siriravalid@users.noreply.github.com>
2024-08-09 22:23:53 -0500

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -6,18 +6,27 @@ from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.text import Tokenizer
 
+# Load dataset
 data = pd.read_csv('Test.csv')
 texts = data['text']
 labels = data['label']
+
+# Split data
 X_train, X_test, y_train, y_test = train_test_split(texts, labels, test_size=0.2, random_state=42)
+
+# Tokenization
 tokenizer = Tokenizer(num_words=5000)
 tokenizer.fit_on_texts(X_train)
 X_train_seq = tokenizer.texts_to_sequences(X_train)
 X_test_seq = tokenizer.texts_to_sequences(X_test)
 X_train_pad = pad_sequences(X_train_seq, maxlen=200)
 X_test_pad = pad_sequences(X_test_seq, maxlen=200)
+
+# Convert labels to categorical
 y_train_cat = to_categorical(y_train)
 y_test_cat = to_categorical(y_test)
+
+# Define and compile model
 model = Sequential([
     Embedding(input_dim=5000, output_dim=128, input_length=200),
     LSTM(128, return_sequences=True),
@@ -25,7 +34,11 @@ model = Sequential([
     Dense(2, activation='softmax')
 ])
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train model
 model.fit(X_train_pad, y_train_cat, epochs=5, validation_data=(X_test_pad, y_test_cat))
+
+# Save model and tokenizer
 model.save('sentiment_model.h5')
 with open('tokenizer.pkl', 'wb') as f:
     import pickle
