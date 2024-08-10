f068db64c50d65f98d641ed108c2a202326784a9
siriravalid <152952587+siriravalid@users.noreply.github.com>
2024-08-09 22:24:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,29 @@ import streamlit as st
 from tensorflow.keras.models import load_model
 import pickle
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# Load the trained model and tokenizer
 model = load_model('sentiment_model.h5')
 with open('tokenizer.pkl', 'rb') as f:
     tokenizer = pickle.load(f)
-st.title('Sentiment Analysis App- Siri')
+
+# Streamlit app
+st.title('Sentiment Analysis App')
 st.write('Enter a text to get sentiment prediction.')
+
+# Input text
 user_input = st.text_area("Review Text")
+
 if st.button('Predict'):
     if user_input:
+        # Preprocess the input text
         input_seq = tokenizer.texts_to_sequences([user_input])
         input_pad = pad_sequences(input_seq, maxlen=200)
+        
+        # Predict sentiment
         prediction = model.predict(input_pad)
         sentiment = 'Positive' if prediction[0][1] > prediction[0][0] else 'Negative'
+        
         st.write(f'Sentiment: {sentiment}')
     else:
-        st.write("Please enter for a review.")
+        st.write("Please enter a review.")
